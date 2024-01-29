d1b5532ba391b7ead734e62d375af23082de0d72
Hadrien <fonguetaghuo@gmail.com>
2024-01-29 02:58:37 +0000

    add jobs to pipeline

--- terraform/ecr/main.tf
+++ terraform/ecr/main.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "remote" {
-    organization = "" # Enter the Terraform Cloud Organization here
+    organization = "arm-aws-ecs-circleci" # Enter the Terraform Cloud Organization here
 
     workspaces {
       name = "ecr-aws-circleci"
